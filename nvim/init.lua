7d10cdcc928e011193c09779418f8584112c252d
RUIJIE.HAO <epona@hey.com>
2021-12-16 09:21:57 +0900

    wip

--- nvim/init.lua
+++ nvim/init.lua
@@ -2,7 +2,6 @@
 local use = require('packer').use
 require('packer').startup(function()
   use 'wbthomason/packer.nvim'       -- Package manager
-  use { 'TimUntersberger/neogit', requires = 'nvim-lua/plenary.nvim' } -- Git management
   use 'tpope/vim-surround'
   use 'mattn/emmet-vim'
 
@@ -13,9 +12,25 @@ require('packer').startup(function()
   -- Add indentation guides even on blank lines
   use { 'lukas-reineke/indent-blankline.nvim' }
   -- Add git related info in the signs columns and popups
-  use {'lewis6991/gitsigns.nvim', requires = {'nvim-lua/plenary.nvim'},   config = function()
-    require('gitsigns').setup()
-  end }
+  use {
+    'TimUntersberger/neogit',
+    requires = 'nvim-lua/plenary.nvim'
+  }
+  use {
+    'lewis6991/gitsigns.nvim',
+    requires = {'nvim-lua/plenary.nvim'},
+    config = function()
+      require('gitsigns').setup()
+    end 
+  }
+  use {
+    'ruifm/gitlinker.nvim',
+    requires = 'nvim-lua/plenary.nvim',
+    config = function ()
+      require('gitlinker').setup()
+    end
+  }
+
   use 'neovim/nvim-lspconfig'        -- Collection of configurations for built-in LSP client
   use 'folke/tokyonight.nvim'
   use { 'hoob3rt/lualine.nvim', requires = {'kyazdani42/nvim-web-devicons', opt = true} }
