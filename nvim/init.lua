9345da07db6d52d78234f8385f5614903df5809b
RUIJIE.HAO <epona@hey.com>
2021-12-22 08:11:05 +0900

    move telescope to separate file

--- nvim/init.lua
+++ nvim/init.lua
@@ -3,6 +3,7 @@ require "user.keymaps"
 require "user.plugins"
 require "user.colorscheme"
 require "user.cmp"
+require "user.telescope"
 
 vim.g.dbs = {
   dev =  'postgres://postgres:secret@127.0.0.1:5432/comiru'
@@ -24,40 +25,6 @@ vim.g.indent_blankline_char = "│"
 vim.g.indent_blankline_filetype_exclude = { 'help', 'packer' }
 vim.g.indent_blankline_buftype_exclude = { 'terminal', 'nofile'}
 vim.g.indent_blankline_char_highlight = 'LineNr'
-
--- Telescope
-require('telescope').setup {
-  defaults = {
-    file_ignore_patterns = {
-       "node_modules/.*",
-       "deps/.*",
-       "_build/.*",
-       "dotbot/.*",
-       ".git/*"
-    },
-    mappings = {
-      i = {
-        ["<C-u>"] = false,
-        ["<C-d>"] = false,
-      },
-    },
-    generic_sorter =  require'telescope.sorters'.get_fzy_sorter,
-    file_sorter =  require'telescope.sorters'.get_fzy_sorter,
-  }
-}
---Add leader shortcuts
-vim.api.nvim_set_keymap('n', '<leader>f', [[<cmd>lua require('telescope.builtin').find_files()<cr>]], { noremap = true, silent = true})
-vim.api.nvim_set_keymap('n', '<leader><space>', [[<cmd>lua require('telescope.builtin').buffers()<cr>]], { noremap = true, silent = true})
-vim.api.nvim_set_keymap('n', '<leader>l', [[<cmd>lua require('telescope.builtin').current_buffer_fuzzy_find()<cr>]], { noremap = true, silent = true})
-vim.api.nvim_set_keymap('n', '<leader>t', [[<cmd>lua require('telescope.builtin').tags()<cr>]], { noremap = true, silent = true})
-vim.api.nvim_set_keymap('n', '<leader>?', [[<cmd>lua require('telescope.builtin').oldfiles()<cr>]], { noremap = true, silent = true})
-vim.api.nvim_set_keymap('n', '<leader>sd', [[<cmd>lua require('telescope.builtin').grep_string()<cr>]], { noremap = true, silent = true})
-vim.api.nvim_set_keymap('n', '<leader>sp', [[<cmd>lua require('telescope.builtin').live_grep()<cr>]], { noremap = true, silent = true})
-vim.api.nvim_set_keymap('n', '<leader>o', [[<cmd>lua require('telescope.builtin').tags{ only_current_buffer = true }<cr>]], { noremap = true, silent = true})
-vim.api.nvim_set_keymap('n', '<leader>gc', [[<cmd>lua require('telescope.builtin').git_commits()<cr>]], { noremap = true, silent = true})
-vim.api.nvim_set_keymap('n', '<leader>gb', [[<cmd>lua require('telescope.builtin').git_branches()<cr>]], { noremap = true, silent = true})
-vim.api.nvim_set_keymap('n', '<leader>gs', [[<cmd>lua require('telescope.builtin').git_status()<cr>]], { noremap = true, silent = true})
-vim.api.nvim_set_keymap('n', '<leader>gp', [[<cmd>lua require('telescope.builtin').git_bcommits()<cr>]], { noremap = true, silent = true})
 
 -- Change preview window location
 vim.g.splitbelow = true
---
+++ nvim/lua/user/telescope.lua
@@ -0,0 +1,43 @@
+local status_ok, telescope = pcall(require, "telescope")
+if not status_ok then
+ return
+end
+
+local actions = require("telescope.actions")
+
+telescope.setup {
+  defaults = {
+    file_ignore_patterns = {
+       "node_modules/.*",
+       "deps/.*",
+       "_build/.*",
+       "dotbot/.*",
+       ".git/*"
+    },
+    mappings = {
+      i = {
+        ["<esc>"] = actions.close,
+        ["<C-u>"] = false,
+        ["<C-d>"] = false,
+      },
+    },
+    generic_sorter =  require'telescope.sorters'.get_fzy_sorter,
+    file_sorter =  require'telescope.sorters'.get_fzy_sorter,
+  }
+} 
+
+local keymap = vim.api.nvim_set_keymap
+local builtin = require("telescope.builtin")
+local options = { noremap = true, silent = true}
+
+--Add leader shortcuts
+keymap('n', '<leader>f', "<cmd>lua require('telescope.builtin').find_files()<cr>", options)
+keymap('n', '<leader><space>', "<cmd>lua require('telescope.builtin').buffers()<cr>", options)
+keymap('n', '<leader>l', "<cmd>lua require('telescope.builtin').current_buffer_fuzzy_find()<cr>", options)
+keymap('n', '<leader>?', "<cmd>lua require('telescope.builtin').oldfiles()<cr>", options)
+keymap('n', '<leader>sd', "<cmd>lua require('telescope.builtin').grep_string()<cr>", options)
+keymap('n', '<leader>sp', "<cmd>lua require('telescope.builtin').live_grep()<cr>", options)
+keymap('n', '<leader>gc', "<cmd>lua require('telescope.builtin').git_commits()<cr>", options)
+keymap('n', '<leader>gb', "<cmd>lua require('telescope.builtin').git_branches()<cr>", options)
+keymap('n', '<leader>gs', "<cmd>lua require('telescope.builtin').git_status()<cr>", options)
+keymap('n', '<leader>gp', "<cmd>lua require('telescope.builtin').git_bcommits()<cr>", options)
