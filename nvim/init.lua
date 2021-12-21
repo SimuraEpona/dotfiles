3708bbcc8be7b6e24bb89bf5172144d5af674035
RUIJIE.HAO <epona@hey.com>
2021-12-22 08:20:04 +0900

    tree-sitter

--- nvim/init.lua
+++ nvim/init.lua
@@ -4,6 +4,7 @@ require "user.plugins"
 require "user.colorscheme"
 require "user.cmp"
 require "user.telescope"
+require "user.treesitter"
 
 vim.g.dbs = {
   dev =  'postgres://postgres:secret@127.0.0.1:5432/comiru'
