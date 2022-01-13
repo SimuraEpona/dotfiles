91f46f904984ce9b27850d840db35f8c2399b4ae
RUIJIE.HAO <epona@hey.com>
2022-01-13 21:06:00 +0900

    use vim-fugitive

--- nvim/lua/user/plugins.lua
+++ nvim/lua/user/plugins.lua
@@ -52,24 +52,7 @@ return packer.startup(function(use)
   -- Add indentation guides even on blank lines
   use { 'lukas-reineke/indent-blankline.nvim' }
   -- Add git related info in the signs columns and popups
-  use {
-    'TimUntersberger/neogit',
-    requires = 'nvim-lua/plenary.nvim'
-  }
-  use {
-    'lewis6991/gitsigns.nvim',
-    requires = {'nvim-lua/plenary.nvim'},
-    config = function()
-      require('gitsigns').setup()
-    end 
-  }
-  use {
-    'ruifm/gitlinker.nvim',
-    requires = 'nvim-lua/plenary.nvim',
-    config = function ()
-      require('gitlinker').setup()
-    end
-  }
+  use 'tpope/vim-fugitive'
 
   use 'neovim/nvim-lspconfig'        -- Collection of configurations for built-in LSP client
   use 'folke/tokyonight.nvim'
