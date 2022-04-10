ebe875c6f009edbc65a8ca4257b08531af581516
RUIJIE.HAO <epona@hey.com>
2022-04-11 07:33:08 +0900

    wip

--- nvim/lua/user/treesitter.lua
+++ nvim/lua/user/treesitter.lua
@@ -10,14 +10,10 @@ require'nvim-treesitter.configs'.setup {
     "json",
     "lua",
     "yaml"
-  }, -- one of "all", "maintained" (parsers with maintainers), or a list of languages
-  ignore_install = { "kotlin" }, -- List of parsers to ignore installing
+  },
   highlight = {
     enable = true,              -- false will disable the whole extension
     disable = { "c", "kotlin", "latex" },  -- list of language that will be disabled
   },
 }
-
-vim.wo.foldmethod = "expr"
-vim.wo.foldexpr = "nvim_treesitter#foldexpr()"
 
