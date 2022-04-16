7957d51666bbc1b88b68a183582337e7b5b23b50
RUIJIE.HAO <epona@hey.com>
2022-04-16 17:08:01 +0900

    setup lua development

--- nvim/lua/user/lsp/init.lua
+++ nvim/lua/user/lsp/init.lua
@@ -36,6 +36,7 @@ vim.cmd([[ command! Format execute 'lua vim.lsp.buf.formatting()' ]])
 local lsp_installer = require("nvim-lsp-installer")
 
 local servers = {
+  "sumneko_lua",
   "elixirls",
   "cssls",
   "tsserver",
@@ -63,6 +64,11 @@ lsp_installer.on_server_ready(function(server)
       local jsonls_opts = require("user.lsp.settings.jsonls")
       opts = vim.tbl_deep_extend("force", jsonls_opts, opts)
     end
+
+     if server.name == "sumneko_lua" then
+      local sumneko_opts = require("user.lsp.settings.sumneko_lua")
+      opts = vim.tbl_deep_extend("force", sumneko_opts, opts)
+     end
 
     -- if server.name == "tailwindcss" then
     --   opts.root_dir = util.root_pattern('tailwind.config.js', 'assets/tailwind.config.js', 'tailwind.config.ts', 'postcss.config.js', 'postcss.config.ts', 'package.json', 'node_modules', '.git')
