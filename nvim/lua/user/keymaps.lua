482f58dbcd1c94a48ce1b04d5986f6802a271bfc
RUIJIE.HAO <epona@hey.com>
2022-04-16 17:03:29 +0900

    use default api

--- nvim/lua/user/keymaps.lua
+++ nvim/lua/user/keymaps.lua
@@ -1,12 +1,5 @@
 local opts = { noremap = true, silent = true }
 
-local term_opts = { silent = true }
-
--- Shorten function name
-local keymap = vim.api.nvim_set_keymap
-
---Remap space as leader key
-keymap("", "<Space>", "<Nop>", opts)
 vim.g.mapleader = " "
 vim.g.maplocalleader = " "
 
@@ -20,26 +13,20 @@ vim.g.maplocalleader = " "
 
 -- Normal --
 -- Better window navigation
-keymap("n", "<Leader>vsp", ":vsp<cr>", opts)
-keymap("n", "<Leader>hsp", ":split<cr>", opts)
-keymap("n", "<C-J>", "<C-W><C-J>", opts)
-keymap("n", "<C-K>", "<C-W><C-K>", opts)
-keymap("n", "<C-H>", "<C-W><C-H>", opts)
-keymap("n", "<C-L>", "<C-W><C-L>", opts)
+vim.keymap.set("n", "<Leader>vsp", ":vsp<cr>", opts)
+vim.keymap.set("n", "<Leader>hsp", ":split<cr>", opts)
+vim.keymap.set("n", "<C-J>", "<C-W><C-J>", opts)
+vim.keymap.set("n", "<C-K>", "<C-W><C-K>", opts)
+vim.keymap.set("n", "<C-H>", "<C-W><C-H>", opts)
+vim.keymap.set("n", "<C-L>", "<C-W><C-L>", opts)
 
 -- Resize with arrows
-keymap("n", "<C-Up>", ":resize +2<CR>", opts)
-keymap("n", "<C-Down>", ":resize -2<CR>", opts)
-keymap("n", "<C-Left>", ":vertical resize -2<CR>", opts)
-keymap("n", "<C-Right>", ":vertical resize +2<CR>", opts)
-
---Remap for dealing with word wrap
-keymap('n', 'k', "v:count == 0 ? 'gk' : 'k'", {noremap=true, expr = true, silent = true})
-keymap('n', 'j', "v:count == 0 ? 'gj' : 'j'", {noremap= true, expr = true, silent = true})
+vim.keymap.set("n", "<C-Up>", ":resize +2<CR>", opts)
+vim.keymap.set("n", "<C-Down>", ":resize -2<CR>", opts)
+vim.keymap.set("n", "<C-Left>", ":vertical resize -2<CR>", opts)
+vim.keymap.set("n", "<C-Right>", ":vertical resize +2<CR>", opts)
 
 -- Y yank until the end of line
-keymap('n', 'Y', 'y$', opts)
+vim.keymap.set('n', 'Y', 'y$', opts)
 
--- Insert --
--- press jj to quit insert mode
-vim.api.nvim_set_keymap("i", "jj", "<Esc>", {})
+vim.keymap.set('i', "jj", "<Esc>")
